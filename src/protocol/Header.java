9c2e662bc50d8d56d4b52eae3069f08303109bf9
ttottochi <117351120+ttottochi@users.noreply.github.com>
2022-12-06 21:02:43 +0900

    헤더 추가 insert menu

--- src/protocol/Header.java
+++ src/protocol/Header.java
@@ -69,8 +69,8 @@ public class Header implements MySerializableClass {
     public final static byte CODE_UPDATE_MENU_QUANTITY = 0x55;
     public final static byte CODE_INSERT_ORDER_MENU = 0x56;
     public final static byte CODE_INSERT_ORDER_OPTION = 0x57;
-    public final static byte CODE_INSERT_MENU = 0x58;
-
+    public final static byte CODE_USER_LIST = 0x58;
+    public final static byte CODE_INSERT_MENU = 0x59;
 
     //TYPE : 0x03(RESULT) 일 때
     public final static byte CODE_SUCCESS = 0x01;
