77e6b68b12562cd0cd2ce53dcd6d503e2752fb9f
Mar1boroo <dnld1759@gmail.com>
2022-12-06 19:07:17 +0900

    통계 정보

--- src/control/StatisticsController.java
+++ src/control/StatisticsController.java
@@ -1,10 +1,13 @@
 package control;
 
+import persistence.StatisticalInfoDTO;
 import protocol.Header;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StatisticsController {
@@ -17,6 +20,31 @@ public class StatisticsController {
                 Header.CODE_STATISTICS,
                 0);
         outputStream.write(startHeader.getBytes());
+
+        // statisticalinfo DTO LIST 받기
+
+        Header list_header = Header.readHeader(inputStream);
+        int listSize = inputStream.readInt();
+        List<StatisticalInfoDTO> list  = new ArrayList<StatisticalInfoDTO>();
+        for(int i = 0 ; i <listSize ; i ++)
+        {
+            int store_id = inputStream.readInt(); String store_name = inputStream.readUTF();
+            String menu_name = inputStream.readUTF(); long sum_order_price = inputStream.readLong();
+            int count_order = inputStream.readInt() ;
+
+            StatisticalInfoDTO statisticalInfoDTO = new StatisticalInfoDTO(store_id , store_name , menu_name , sum_order_price , count_order);
+            list.add(statisticalInfoDTO);
+        }
+
+        System.out.println("메뉴 별 통계");
+        for(int i = 0 ; i < list.size() ; i ++)
+        {
+            StatisticalInfoDTO statisticInfo = list.get(i);
+            System.out.println(statisticInfo.getMenu_name()+" | 총 매출 :"+statisticInfo.getSum_order_price()+
+                    " | 주문 수 : " + statisticInfo.getCount_order());
+        }
+
+
 
 
     }
---
+++ src/persistence/StatisticalInfoDTO.java
@@ -0,0 +1,44 @@
+package persistence;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import protocol.MySerializableClass;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+@Getter
+@Setter
+@ToString
+
+public class StatisticalInfoDTO implements MySerializableClass {
+    private int store_id;
+    private String store_name;
+    private String menu_name;
+    private long sum_order_price;
+    private int count_order;
+
+    public StatisticalInfoDTO(int store_id, String store_name, String menu_name, long sum_order_price, int count_order)
+    {
+        this.store_id = store_id;
+        this.store_name = store_name;
+        this.menu_name = menu_name;
+        this.sum_order_price = sum_order_price;
+        this.count_order = count_order;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(buf);
+
+        dos.writeInt(store_id);
+        dos.writeUTF(store_name);
+        dos.writeUTF(menu_name);
+        dos.writeLong(sum_order_price);
+        dos.writeInt(count_order);
+        return buf.toByteArray();
+    }
+}
